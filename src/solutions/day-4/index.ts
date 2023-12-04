079bf6d64fd4f843a6724b9d6c86412cb8912a95
Liam Canetti <git@liamcanetti.co.uk>
2023-12-04 19:46:00 +0000

    feat(day-4): part 2 fin

--- src/solutions/day-4/index.ts
+++ src/solutions/day-4/index.ts
@@ -2,12 +2,17 @@ import { readAndSplitFile } from "../../helpers/readFile.helper";
 
 const games = readAndSplitFile("day-four").split("\n");
 
+const getWinningAndDrawnNumbers = (gameString: string) => {
+	const game = gameString.split(":")[1];
+	const [winningNumbers, drawnNumbers] = game.split("|").map((item) => item.split(" ").filter(Boolean));
+
+	return { winningNumbers, drawnNumbers, playCount: 1 };
+};
+
 const solutionOne = () => {
 	return games.reduce((acc, curr) => {
-		const game = curr.split(":")[1];
-		const [winningNumbers, drawnNumbers] = game.split("|").map((item) => item.split(" ").filter(Boolean));
+		const { winningNumbers, drawnNumbers } = getWinningAndDrawnNumbers(curr);
 		const winnersCount = drawnNumbers.filter((number) => winningNumbers.includes(number)).length;
-
 		if (winnersCount === 0) return acc;
 		if (winnersCount === 1) return acc + 1;
 		return acc + Math.pow(2, winnersCount - 1);
@@ -15,7 +20,21 @@ const solutionOne = () => {
 };
 
 const solutionTwo = () => {
-	return "todo";
+	const gamesWithWins = games.map((game) => getWinningAndDrawnNumbers(game));
+
+	gamesWithWins.forEach((game, index) => {
+		const { winningNumbers, drawnNumbers, playCount } = game;
+		const winnersCount = drawnNumbers.filter((number) => winningNumbers.includes(number)).length;
+
+		if (winnersCount === 0) return;
+
+		Array.from({ length: winnersCount }).forEach((_, wins) => {
+			const cardIndex = index + wins + 1;
+			if (gamesWithWins[cardIndex]) gamesWithWins[cardIndex].playCount += playCount;
+		});
+	});
+
+	return gamesWithWins.reduce((acc, curr) => acc + curr.playCount, 0);
 };
 
 export const dayFourAnswers = () => {
