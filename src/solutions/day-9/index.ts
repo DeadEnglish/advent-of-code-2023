7e1e8861d4bd68612bfa1ad9aefff412c3c5aee4
Liam Canetti <git@liamcanetti.co.uk>
2023-12-09 11:47:38 +0000

    feat(day-9): reverse, slide to the left

--- src/solutions/day-9/index.ts
+++ src/solutions/day-9/index.ts
@@ -1,12 +1,39 @@
 import { readFile } from "../../helpers/readFile.helper";
 
-const puzzle = readFile("day-9").split("\n");
+const lines = readFile("day-9").split("\n");
+
+const mapLines = lines.map((line) => {
+	return line.split(" ").map(Number);
+});
+
+const findDifference = (numbers: number[]): number[] => {
+	return numbers
+		.map((number, index) => {
+			if (index <= numbers.length) {
+				return numbers[index + 1] - number;
+			}
+		})
+		.filter((num) => !isNaN(num!)) as number[];
+};
+
+const findNextSequenceValue = (numbers: number[]): number => {
+	if (numbers.every((num) => num === numbers[0])) {
+		return numbers[0];
+	}
+
+	const difference = findDifference(numbers);
+	return numbers[numbers.length - 1] + findNextSequenceValue(difference);
+};
+
 const solutionOne = () => {
-	return "todo";
+	return mapLines.map(findNextSequenceValue).reduce((acc, curr) => acc + curr, 0);
 };
 
 const solutionTwo = () => {
-	return "todo";
+	return mapLines
+		.map((line) => line.reverse())
+		.map(findNextSequenceValue)
+		.reduce((acc, curr) => acc + curr, 0);
 };
 
 export const dayNineAnswers = () => {
